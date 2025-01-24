c77970f1f6d720e81cf2988dad889f74054d3e87
francescapieruz16 <francescadora.pieruz@mail.polimi.it>
2025-01-24 16:39:48 +0000

    added build and made some changes on the code

---
+++ build/CMakeFiles/project.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project.dir/src/WaveSolver.cpp.o"
+  "CMakeFiles/project.dir/src/WaveSolver.cpp.o.d"
+  "CMakeFiles/project.dir/src/project.cpp.o"
+  "CMakeFiles/project.dir/src/project.cpp.o.d"
+  "project"
+  "project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
